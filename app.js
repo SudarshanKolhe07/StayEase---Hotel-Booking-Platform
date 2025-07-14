3de276eefc81aad75ecf4735aee5c9c28758d5d5
SudarshanKolhe07 <sudarshankolhe04@gmail.com>
2025-07-14 20:13:12 +0530

    Fix: Preserve image on listing update, adjust validation schema, and improve image display

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose= require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride =require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema,reviewSchema} = require("./schema.js");
 const { error } = require("console");
-const Review = require("./models/reviews.js");
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/reviews.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wander";
 
@@ -36,94 +35,8 @@ app.get("/", (req,res) => {
     res.send("Hii, I am root");
 });
 
-const validateListing = (req,res,next)=>{
-    let {error} = listingSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }else{
-        next();
-    }
-};
-
-const validateReview = (req,res,next)=>{
-    let {error} = reviewSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }else{
-        next();
-    }
-};
-
-//Index route
-app.get("/listings", wrapAsync(async(req,res)=>{
-    let allListings=await Listing.find({});
-    res.render("listings/index.ejs",{allListings});
-}));
-
-//new route
-app.get("/listings/new", (req,res)=>{
-    res.render("listings/new.ejs");
-})
-
-//show route
-app.get("/listings/:id", wrapAsync(async(req,res)=>{
-    let {id}= req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs",{listing});
-}));
-
-//create route
-app.post("/listings", validateListing,wrapAsync(async (req, res, next) => {
-    const newListing = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-}));
-
-
-//edit route
-app.get("/listings/:id/edit", wrapAsync(async(req,res)=>{
-    let {id}= req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-}));
-
-// update route
-app.put("/listings/:id", validateListing,wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}));
-
-//Delete Route
-app.delete("/listings/:id", wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    let deleteListing=await Listing.findByIdAndDelete(id);
-    console.log(deleteListing);
-    res.redirect("/listings");
-}));
-
-//Review Post route
-app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
-    let listing = await Listing.findById(req.params.id);
-    let newReview=new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listings/${listing._id}`);
-}));
-
-// Delete Review route
-app.delete('/listings/:id/reviews/:reviewsId',wrapAsync(async(req,res)=>{
-    let {id, reviewsId} = req.params;
-    await Listing.findByIdAndUpdate(id,{$pull:{reviews: reviewsId}});
-    await Review.findByIdAndDelete(reviewsId);
-
-    res.redirect(`/listings/${id}`);
-}));
+app.use("/listings", listings);
+app.use("listings/:id/reviews", reviews);
 
 
 app.all("*",(req,res,next)=>{
