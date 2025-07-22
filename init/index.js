d6cc5eaa140c52f314277a9eb094fb1247009c25
SudarshanKolhe07 <sudarshankolhe04@gmail.com>
2025-07-22 19:56:40 +0530

    feat: improve listing display in show.ejs and add authentication check in listing.js

--- init/index.js
+++ init/index.js
@@ -18,6 +18,10 @@ async function main(){
 
 const initDB = async () =>{
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj)=>({
+        ...obj,
+        owner:"687a98985ae175210d970c28",
+    }));
     await Listing.insertMany(initData.data);
     console.log("Data was initialized.");
 }
