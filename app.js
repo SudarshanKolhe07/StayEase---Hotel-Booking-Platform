22c1faed929928243d0ae11fa2b6525703c7fa92
SudarshanKolhe07 <sudarshankolhe04@gmail.com>
2025-09-04 17:18:41 +0530

    Updated listing information

--- app.js
+++ app.js
@@ -1,162 +1,124 @@
-if(process.env.NODE_ENV != "production"){
+if (process.env.NODE_ENV != "production") {
     require("dotenv").config();
 }
 
 const express = require("express");
 const app = express();
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 const path = require("path");
-const methodOverride =require("method-override");
+const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-const { error } = require("console");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
 const session = require("express-session");
-const MongoStore = require('connect-mongo');
-
+const MongoStore = require("connect-mongo");
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/reviews.js");
 const userRouter = require("./routes/user.js");
-const helmet = require("helmet");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wander";
+// MongoDB Atlas URL
 const dbURL = process.env.ATLASDB_URL;
 
 main()
-    .then(()=>{
+    .then(() => {
         console.log("Connected to DB..");
     })
-    .catch((err)=>{
-        console.log("err");
+    .catch((err) => {
+        console.log("err", err);
     });
 
-async function main(){
-    mongoose.connect(dbURL); 
-}
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended:true}));
-app.use(methodOverride("_method"));
-app.engine('ejs',ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
-
-const store = MongoStore.create({
-    mongoUrl: dbURL,
-    crypto:{
-        secret:process.env.SECRET,
-    },
-    touchAfter:24*3600,
+async function main() {
+    mongoose.connect(process.env.ATLASDB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-store.on("error",()=>{
+
+}
+
+
+// App settings
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+// Mongo session store
+const store = MongoStore.create({
+    mongoUrl: dbURL,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error", (err) => {
     console.log("ERROR IN MONGO SESSION STORE", err);
 });
 
-const sessionOptions ={
+const sessionOptions = {
     store,
-    secret : process.env.SECRET,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie:{
-        expires:Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge:7 * 24 * 60 * 60 * 1000,
-        httpOnly:true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
     },
 };
 
-// app.get("/", (req,res) => {
-//     res.send("Hii, I am root");
-// });
-
-
-// Force HTTPS (important for Render)
-app.enable("trust proxy");
-app.use((req, res, next) => {
-  if (req.secure) {
-    next();
-  } else {
-    res.redirect(`https://${req.headers.host}${req.url}`);
-  }
-});
-
-// Helmet security middleware
-app.use(
-  helmet({
-    contentSecurityPolicy: false, // disable CSP first if breaking scripts, can be configured later
-  })
-);
-
-// Stronger CSP (optional, but helps)
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: [
-        "'self'",
-        "https://cdn.jsdelivr.net", // example for external scripts
-        "https://api.mapbox.com",
-        "'unsafe-inline'" // only if required for inline JS
-      ],
-      styleSrc: [
-        "'self'",
-        "https://fonts.googleapis.com",
-        "'unsafe-inline'"
-      ],
-      fontSrc: ["'self'", "https://fonts.gstatic.com"],
-      imgSrc: ["'self'", "data:", "https://res.cloudinary.com"], // add your Cloudinary
-      connectSrc: ["'self'", "https://api.mapbox.com"],
-    },
-  })
-);
-
-
+// Middleware
 app.use(session(sessionOptions));
-app.use(flash()); 
+app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
-
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-    res.locals.success  = req.flash("success");
-    res.locals.error  = req.flash("error");
-    res.locals.currUser=req.user || null;
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user || null;
     next();
 });
 
-app.get("/demouser", async(req,res)=>{
+// Demo user route
+app.get("/demouser", async (req, res) => {
     let fakeUser = new User({
         email: "abc@gmail.com",
-        username:"SudarshanKolhe",
+        username: "SudarshanKolhe",
     });
-
     let newUser = await User.register(fakeUser, "helloworld");
     res.send(newUser);
-})
+});
 
+// Routers
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-
-app.all("*",(req,res,next)=>{
-    next(new ExpressError(404,"page not found!"));
+// 404 handler
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "page not found!"));
 });
 
+// Error handler
 app.use((err, req, res, next) => {
-    let {statusCode = 500, message = "Something went wrong!"} = err;
+    let { statusCode = 500, message = "Something went wrong!" } = err;
     res.status(statusCode).render("error.ejs", { message });
 });
 
+// Server
 app.listen(8080, () => {
     console.log("Server is listening to port 8080...");
 });
